f9757acb7bde4e91745e1d6e18cb0b4b094e931b
shotanue <shotanue@gmail.com>
2021-08-28 00:37:27 +0900

    remove sample implementations

--- src/components/Button.spec.ts
+++
@@ -1,7 +0,0 @@
-import { mount } from '@cypress/vue';
-import Button from '@/components/Button.vue';
-
-it('Button', () => {
-  mount(Button);
-  cy.findByRole('button', { name: 'Test button' }).click();
-});
