9f7146ebb31b71b0ca4056c0068b5cea95d20e9d
shotanue <shotanue@gmail.com>
2021-09-12 23:34:06 +0900

    rename named export functions

--- src/renderers/controls/stringControlRenderer/stringControlRendererEntry.ts
+++ src/renderers/controls/stringControlRenderer/stringControlRendererEntry.ts
@@ -1,7 +1,7 @@
 import { isStringControl, JsonFormsRendererRegistryEntry, rankWith } from '@jsonforms/core';
 import renderer from '@/renderers/controls/stringControlRenderer/stringControlRenderer.vue';
 
-export const entry: JsonFormsRendererRegistryEntry = {
+export const stringControlRendererEntry: JsonFormsRendererRegistryEntry = {
   renderer,
   tester: rankWith(1, isStringControl),
 };
