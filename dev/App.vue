2a79c5f57ba6bc08c6f2265b9c07810316e242cc
shotanue <shotanue@gmail.com>
2022-01-21 01:48:00 +0900

    chore: make build result debuggable

--- dev/App.vue
+++ dev/App.vue
@@ -1,7 +1,9 @@
 <template>
   <span>{{ text }}</span>
+  <JsonForms />
 </template>
 
 <script lang="ts" setup>
+  import JsonForms from './JsonForms.vue';
   const text = 'hello world';
 </script>
---
+++ dev/JsonForms.vue
@@ -0,0 +1,22 @@
+<template>
+  <json-forms :data="data" :schema="schema" :uischema="uischema" :renderers="renderers" @change="onChagne" />
+</template>
+
+<script setup>
+  import { JsonForms } from '@jsonforms/vue';
+  import { markRaw } from 'vue';
+  import { antdvRenderers } from '../dist/jsonforms-antdv.es';
+
+  import uischema from './uischema.json';
+  import schema from './schema.json';
+  const renderers = markRaw([...antdvRenderers]);
+  let data = {
+    properties: {
+      foo: '',
+    },
+  };
+
+  const onChagne = event => {
+    data = event.data;
+  };
+</script>
