028bbf2d9143f176ca48d54f49605fe1d071602d
shotanue <shotanue@gmail.com>
2022-02-17 01:06:45 +0900

    feat: support options.suggestion in stringRenderer

--- src/renderers/controls/stringControlRenderer/stringControlRenderer.vue
+++ src/renderers/controls/stringControlRenderer/stringControlRenderer.vue
@@ -1,6 +1,18 @@
 <template>
   <control-wrapper :form-item-bind="controlWrapperBind">
-    <a-input v-bind="inputBind" @update:value="updateValue" />
+    <a-input v-if="suggestions.length === 0" v-bind="inputBind" @update:value="updateValue" />
+    <a-auto-complete
+      v-else-if="suggestions.length > 0"
+      v-bind="inputBind"
+      @update:value="updateValue"
+      @search="onSearch"
+    >
+      <template #options>
+        <a-select-option v-for="option in result" :key="option">
+          {{ option }}
+        </a-select-option>
+      </template>
+    </a-auto-complete>
   </control-wrapper>
 </template>
 
@@ -9,6 +21,8 @@
   import type { ControlElement } from '@jsonforms/core';
   import { useControl } from '@/renderers/controls/useControl';
   import ControlWrapper from '@/renderers/controls/controlWrapper.vue';
+  import { ref } from 'vue';
+  import clone from 'just-clone';
 
   const props = defineProps({
     ...rendererProps<ControlElement>(),
@@ -19,4 +33,33 @@
     controlWrapperBind,
     on: { updateValue },
   } = useControl(useJsonFormsControl(props));
+
+  const suggestions = ref<string[]>(
+    (() => {
+      const items = inputBind.value?.suggestion;
+
+      if (items === undefined) {
+        return [];
+      }
+
+      if (!Array.isArray(items)) {
+        throw TypeError('[jsonforms antdv] suggestion type should be Array<string>');
+      }
+      items.forEach(suggestion => {
+        if (typeof suggestion !== 'string') {
+          throw TypeError('[jsonforms antdv] suggestion type should be Array<string>');
+        }
+      });
+
+      return items;
+    })()
+  );
+
+  const result = ref<string[]>(clone(suggestions.value));
+
+  const onSearch = (searchText: string) => {
+    result.value = suggestions.value.filter((x: string) => {
+      return x.toUpperCase().indexOf(searchText.toUpperCase()) >= 0;
+    });
+  };
 </script>
